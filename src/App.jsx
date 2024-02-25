8374ac7c89713933d7e5b1f51a86459dcc0ce5fb
sabbir10s <sabbirahmed1023@gmail.com>
2024-02-25 18:53:59 +0600

    initial project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-green-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center text-red-400">
+        Welcome to Home Page
+      </h2>
+      <h3 className="text-4xl text-center font-poppins text-secondary">
+        Welcome to Home Page
+      </h3>
+    </div>
+  );
+};
+
+export default Home;
